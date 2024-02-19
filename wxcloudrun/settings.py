7307bb0af837bf52687848d91531d03d1106b182
Xianfeng Zhuge <xianfeng19971117@gmail.com>
2024-02-20 03:00:45 +1100

    debug

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -76,15 +76,15 @@ DATABASES = {
         'OPTIONS': {'charset': 'utf8mb4'},
     },
 
-    'local': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-
-    }
+    # 'local': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'mydb',
+    #     'USER': 'postgres',
+    #     'PASSWORD': '123',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    #
+    # }
 }
 
 # Password validation
