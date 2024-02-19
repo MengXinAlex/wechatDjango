8d209deaf1924272798a86489807636a08d55682
Xianfeng Zhuge <xianfeng19971117@gmail.com>
2024-02-20 02:56:35 +1100

    switch back to prod env

--- wxcloudrun/settings.py
+++ wxcloudrun/settings.py
@@ -66,17 +66,17 @@ WSGI_APPLICATION = 'wxcloudrun.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': os.environ.get("MYSQL_DATABASE", 'django_demo'),
-    #     'USER': os.environ.get("MYSQL_USERNAME"),
-    #     'HOST': os.environ.get("MYSQL_ADDRESS").split(':')[0],
-    #     'PORT': os.environ.get("MYSQL_ADDRESS").split(':')[1],
-    #     'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
-    #     'OPTIONS': {'charset': 'utf8mb4'},
-    # },
-
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("MYSQL_DATABASE", 'django_demo'),
+        'USER': os.environ.get("MYSQL_USERNAME"),
+        'HOST': os.environ.get("MYSQL_ADDRESS").split(':')[0],
+        'PORT': os.environ.get("MYSQL_ADDRESS").split(':')[1],
+        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+        'OPTIONS': {'charset': 'utf8mb4'},
+    },
+
+    'local': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mydb',
         'USER': 'postgres',
